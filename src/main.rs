9d3b3fdf5d1b6863fa4308afe78cb4fb85c47d34
Ezequiel <zeki-san@hotmail.com>
2025-09-05 17:26:00 -0300

    vai

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,20 @@ fn panic(_info: &PanicInfo) -> ! {
 	loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 // this function is the entry point, since the linker looks for a function
 // named `_start` by default
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+	let vga_buffer = 0xb8000 as *mut u8;
+
+	for (i, &byte) in HELLO.iter().enumerate() {
+		unsafe {
+			*vga_buffer.offset(i as isize * 2) = byte;
+			*vga_buffer.offset(i as isize * 2 + 1) = 0xb; // light cyan foreground
+		}
+	}
+
 	loop {}
 }
